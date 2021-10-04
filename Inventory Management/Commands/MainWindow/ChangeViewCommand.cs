36bfbb1a0e440c87d8304eeb743ed6d1c8abf9ea
poglodek <pawelogll@gmail.com>
2021-10-04 15:07:47 +0200

    adding change view command

---
+++ Inventory Management/Commands/MainWindow/ChangeViewCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Inventory_Management.ViewModel;
+
+namespace Inventory_Management.Commands.MainWindow
+{
+    public class ChangeViewCommand : ICommand
+    {
+        private MainWindowViewModel viewModel;
+
+        public ChangeViewCommand(MainWindowViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            switch (parameter.ToString())
+            {
+                case "Items":
+                    viewModel.ViewModel = new ItemsViewModel();
+                    break;
+                case "Clients":
+                    viewModel.ViewModel = new ClientsViewModel();
+                    break;
+                case "FV":
+                    viewModel.ViewModel = new FVViewModel();
+                    break;
+                case "MyData":
+                    viewModel.ViewModel = new MyDataViewModel();
+                    break;
+                case "Stocktaking":
+                    viewModel.ViewModel = new StockTakingViewModel();
+                    break;
+                case "Orders":
+                    viewModel.ViewModel = new OrdersViewModel();
+                    break;
+            }
+        }
+
+        public event EventHandler? CanExecuteChanged;
+    }
+}
--- Inventory Management/ViewModel/MainWindowViewModel.cs
+++ Inventory Management/ViewModel/MainWindowViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Inventory_Management.Commands.MainWindow;
 using Inventory_Management.ViewModel.Base;
 
 namespace Inventory_Management.ViewModel
@@ -11,10 +13,20 @@ namespace Inventory_Management.ViewModel
     {
         private ViewModelBase viewModel = new ItemsViewModel();
 
+        public MainWindowViewModel()
+        {
+            UpdateViewCommand = new ChangeViewCommand(this);
+        }
         public ViewModelBase ViewModel
         {
             get => viewModel;
-            set => viewModel = value;
+            set
+            {
+                viewModel = value;
+                OnPropertyChanged(nameof(ViewModel));
+            }
         }
+        public ICommand UpdateViewCommand { get; set; }
+
     }
 }
